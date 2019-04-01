8183929eeab1d4c9c6a99fa2633e63a878ad4e4a
Your Name <leoncyao@gmail.com>
2019-03-31 22:55:22 -0400

    3/31/2019

--- practiceModel.py
+++ practiceModel.py
@@ -3,8 +3,8 @@ from Useful import *
 
 # Q = [[1, 3, 5], [4, 2, 1]]
 # Q = [[1, 3], [2, 4]]
-# Q = [[42]]
-Q = [[math.pi]]
+Q = [[42]]
+# Q = [[math.pi]]
 # Q = [[math.pi, 2.7], [1, 3]]
 
 # Will need to change this later. numInputs should be the
@@ -19,13 +19,12 @@ test_samples = createQTests(numTrials, numTests, numInputs, numOutputs, Q, 1)
 # print(test_samples)
 if __name__ == "__main__":
 	# w = [
-	# 	[[2, 3], [5, 4]], # multiply this row by in 1 by 1 input vector (layer0) to get the layer1 activations
-	# 	[[1, 1], [3, 1]],
-	# 	[[1, 1], [3, 1]] # multiply this row by the 2 by 1 layer1 activation to get output layer
+	# 	[[1, 1], [1, 1]], # multiply this row by in 1 by 1 input vector (layer0) to get the layer1 activations
+	# 	[[1, 1], [1, 1]],
+	# 	[[1, 1], [1, 1]] # multiply this row by the 2 by 1 layer1 activation to get output layer
 	# 	 ]
 	# w = [
-	# 	[[0], [0]],
-	# 	[[0], [0]]
+	# 	[[1, 3], [2, 4]],
 	# 	  ]
 	w = [
 		[[5]],
@@ -67,36 +66,14 @@ if __name__ == "__main__":
 			for i2 in range(len(w[numLayers-2])):
 				dw[numLayers-2][i1][i2] = 2 * (a[numLayers-1][i1] - y[i1]) * a[numLayers-2][i2]
 
-		# can use the d value of the layer in front to calculate backwards
-		# dw[0][0][0] = dw[1][0][0] / a[1][0] * w[1][0][0] * a[0][0]
-		# dw[0][1][0] = dw[1][0][1] / a[1][1] * w[1][0][1] * a[0][0]
 
 		# need to calculate da values to calculate the next dw values
 
-		# layer_num = 1
-
-		# fixing layer number 1
-		# for e1 in range(len(a[1])):
-		# 	for e2 in range(len(a[2])):
-		# 		# da[1][e] += (dw[1][0][0] / a[1][e] * w[1][0][0] + dw[1][1][0] / a[1][e] * w[1][1][0])
-		# 		da[1][e1] += dw[1][e1][e2] / a[1][e1] * w[1][e1][e2]
-
-		#
-		# for e1 in range(len(a[1])):
-		# 	for e2 in range(len(a[2])):
-		# 		dw[layer_num-1][e1][e2] = da[e1][e2] * a[layer_num-1][e2]
-
 		# starts at second last layer, stops at second (first layer is inputs)
 		for layer_num in range(numLayers-2, 0, -1):
-			# for e1 in range(len(a[1])):
-			# 	for e2 in range(len(a[2])):
 			for e1 in range(len(a[layer_num])):
 				for e2 in range(len(a[layer_num+1])):
-					# da[1][e] += (dw[1][0][0] / a[1][e] * w[1][0][0] + dw[1][1][0] / a[1][e] * w[1][1][0])
-					# da[1][e1] += dw[1][e1][e2] / a[1][e1] * w[1][e1][e2]
 					da[layer_num][e1] += dw[layer_num][e1][e2] / a[layer_num][e1] * w[layer_num][e1][e2]
-			# da[1][0] = (dw[1][0][0] / a[1][0] * w[1][0][0] + dw[1][1][0] / a[1][0] * w[1][1][0])
-			# da[1][1] = (dw[1][0][1] / a[1][1] * w[1][0][1] + dw[1][1][1] / a[1][1] * w[1][1][1])
 
 			print(da)
 			for e1 in range(len(a[1])):
@@ -121,12 +98,11 @@ if __name__ == "__main__":
 
 		xVals.append(i)
 		costVals.append(total_cost)
-		# print(total_cost)
 		for q in range(0, numOutputs):
 			output_Vals[q].append(a[numLayers-1][q])
 		for q in range(0, numOutputs):
 			yVals[q].append(y[q])
-	print(w)
+	print("w " + str(w))
 	for i in range(numOutputs):
 		pylab.subplot((numOutputs + 1) * 100 + 11 + i)
 		pylab.plot(xVals, yVals[i], 'b--', label="yVals[{}]".format(i))
@@ -136,7 +112,6 @@ if __name__ == "__main__":
 	pylab.subplot((numOutputs + 1) * 100 + 11 + numOutputs)
 	pylab.plot(xVals, costVals, 'g--')
 	pylab.show()
-	# show_results()
 
 
 
